6db4775c34bccf61b5de7b7b520aa3e2c5f3c48d
yves <kabiryves90@gmail.com>
2020-11-03 13:30:36 +0200

    modification du head

--- resources/views/hnsfs/head.blade.php
+++ resources/views/hnsfs/head.blade.php
@@ -18,4 +18,36 @@
 
     <!-- Custom Theme Style -->
     <link href="{{ asset('build/css/custom.min.css')}}" rel="stylesheet">
+
+	<!-- iCheck -->
+	<link href="{{ asset('vendors/iCheck/skins/flat/green.css')}}" rel="stylesheet">
+	<!-- bootstrap-wysiwyg -->
+	<link href="{{ asset('vendors/google-code-prettify/bin/prettify.min.css')}}" rel="stylesheet">
+	<!-- Select2 -->
+	<link href="{{ asset('vendors/select2/dist/css/select2.min.css')}}" rel="stylesheet">
+	<!-- Switchery -->
+	<link href="{{ asset('vendors/switchery/dist/switchery.min.css')}}" rel="stylesheet">
+	<!-- starrr -->
+	<link href="{{ asset('vendors/starrr/dist/starrr.css')}}" rel="stylesheet">
+	<!-- bootstrap-daterangepicker -->
+	<link href="{{ asset('vendors/bootstrap-daterangepicker/daterangepicker.css')}}" rel="stylesheet">
+
+	<!-- Custom Theme Style -->
+	<link href="{{ asset('build/css/custom.min.css')}}" rel="stylesheet">
+
+
+
+  <!-- Bootstrap -->
+   <link href="{{ asset('cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css')}}">
+
+
+   <!-- Datatables -->
+
+   <link href="{{ asset('vendors/datatables.net-bs/css/dataTables.bootstrap.min.css')}}" rel="stylesheet">
+   <link href="{{ asset('vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css')}}" rel="stylesheet">
+   <link href="{{ asset('vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css')}}" rel="stylesheet">
+   <link href="{{ asset('vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css')}}" rel="stylesheet">
+   <link href="{{ asset('vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css')}}" rel="stylesheet">
+
+  
   </head>
