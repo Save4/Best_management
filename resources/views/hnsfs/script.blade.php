2602643afb44d7e9a3dfc1a76d0414208eb0f569
yves <kabiryves90@gmail.com>
2020-11-03 09:24:24 +0200

    modification du script

--- resources/views/hnsfs/script.blade.php
+++ resources/views/hnsfs/script.blade.php
@@ -1,13 +1,13 @@
 <!-- jQuery -->
-    <script src="{{ asset('css_js/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{ asset('vendors/jquery/dist/jquery.min.js')}}"></script>
     <!-- Bootstrap -->
-   <script src="{{ asset('css_js/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+   <script src="{{ asset('vendors/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
     <!-- FastClick -->
-    <script src="{{ asset('css_js/fastclick/lib/fastclick.js')}}"></script>
+    <script src="{{ asset('vendors/fastclick/lib/fastclick.js')}}"></script>
     <!-- NProgress -->
-    <script src="{{ asset('css_js/nprogress/nprogress.js')}}"></script>
+    <script src="{{ asset('vendors/nprogress/nprogress.js')}}"></script>
     <!-- jQuery custom content scroller -->
-    <script src="{{ asset('css_js/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+    <script src="{{ asset('vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js')}}"></script>
 
     <!-- Custom Theme Scripts -->
-    <script src="{{ asset('css_js/custom.min.js')}}"></script>
+    <script src="{{ asset('build/js/custom.min.js')}}"></script>
