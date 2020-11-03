87825f444010aee1ce07be06e75cce3a2e74f9a3
yves <kabiryves90@gmail.com>
2020-11-03 08:28:24 +0200

    ajout du navbar

---
+++ resources/views/hnsfs/navbar.blade.php
@@ -0,0 +1,31 @@
+ <div class="top_nav">
+            <div class="nav_menu">
+                <div class="nav toggle">
+                  <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                </div>
+                <nav class="nav navbar-nav">
+                <ul class=" navbar-right">
+                  <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+
+                                    <a  class="dropdown-item" href="{{url('register')}}">Enregistre l'utilisateur</a>
+                                </div>
+                            </li>
+
+                </ul>
+              </nav>
+            </div>
+          </div>
