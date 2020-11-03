f62f903c2445f4a272b13cc41a556faf2a9aa7d5
yves <kabiryves90@gmail.com>
2020-11-03 08:28:42 +0200

    ajout du script

---
+++ resources/views/hnsfs/script.blade.php
@@ -0,0 +1,13 @@
+<!-- jQuery -->
+    <script src="{{ asset('css_js/jquery/dist/jquery.min.js')}}"></script>
+    <!-- Bootstrap -->
+   <script src="{{ asset('css_js/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- FastClick -->
+    <script src="{{ asset('css_js/fastclick/lib/fastclick.js')}}"></script>
+    <!-- NProgress -->
+    <script src="{{ asset('css_js/nprogress/nprogress.js')}}"></script>
+    <!-- jQuery custom content scroller -->
+    <script src="{{ asset('css_js/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="{{ asset('css_js/custom.min.js')}}"></script>
