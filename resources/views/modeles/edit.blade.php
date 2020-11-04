46f4684c04a552fb114c1acfe3baf9c772f76cae
yves <kabiryves90@gmail.com>
2020-11-04 11:10:26 +0200

    modification d'une page edit

--- resources/views/modeles/edit.blade.php
+++ resources/views/modeles/edit.blade.php
@@ -1,72 +1,68 @@
 @extends('layouts.layout')
 @section('content')
 
-   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Modele</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Modele</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
+  <div class="clearfix"></div>
+  					<div class="row">
+  						<div class="col-md-12 col-sm-12 ">
+  							<div class="x_panel">
+  								<div class="x_title">
+  									<h2>Modification des modeles</small></h2>
+  									<ul class="nav navbar-right panel_toolbox">
+  										<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+  										</li>
+  										<li class="dropdown">
+  											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-wrench"></i></a>
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <!-- left column -->
-          <div class="col-md-6">
-            <!-- jquery validation -->
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Modifier le modele</h3>
-              </div>
+  										</li>
+  										<li><a class="close-link"><i class="fa fa-close"></i></a>
+  										</li>
+  									</ul>
+  									<div class="clearfix"></div>
+  								</div>
+  								<div class="x_content">
+  									<br />
+  									<form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" action="/modeles/{{$Modele->id}}" method="post">
 
-
-                    <div class="col-md-12">
-                        <form role="form" action="/modeles/{{$Modele->id}}" method="post">
                       @csrf
                       @method('PUT')
+  										<div class="item form-group">
+  											<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Nom du modele <span class="required">*</span>
+  											</label>
+  											<div class="col-md-6 col-sm-6 ">
+  												<input type="text" name="nom_modele" id="first-name" required="required" class="form-control "class="@error('nom_modele') is-invalid @enderror" placeholder=""
+                            value="{{ $Modele->nom_modele }}">
+                        @error('nom_modele')
+                        <button class="btn-danger">{{ $message }}</button>
+                        @enderror
+  											</div>
+  										</div>
 
-                        <div class="form-group">
-                            <label>Modele :</label>
-                            <input type="text" name="nom_modele" class="form-control"
-                                class="@error('nom_modele') is-invalid @enderror" placeholder=""
-                                value="{{ $Modele->nom_modele }}">
-                            @error('nom_modele')
-                            <button class="btn-danger">{{ $message }}</button>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label>Temp :</label>
-                            <input type="text" name="temp_actuel" class="form-control"
-                                class="@error('temp_actuel') is-invalid @enderror" placeholder=""
-                                value="{{ $Modele->temp_actuel }}">
-                            @error('temp_actuel')
-                            <button class="btn-danger">{{ $message }}</button>
-                            @enderror
-                        </div>
+  										<div class="item form-group">
+  											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Date d'enregistrement</label>
+  											<div class="col-md-6 col-sm-6 ">
+  												<input name="temp_actuel" id="middle-name" class="form-control" type="date" name="middle-name"   class="@error('temp_actuel') is-invalid @enderror" placeholder=""
+                            value="{{ $Modele->temp_actuel }}">
+                        @error('temp_actuel')
+                        <button class="btn-danger">{{ $message }}</button>
+                        @enderror
+  											</div>
+  										</div>
+                      <div class="item form-group">
+  											<div class="col-md-6 col-sm-6 offset-md-3">
 
-                        <div class="card-footer">
-                          <button type="submit" class="btn btn-primary">Modifier</button>
-                          <button type="reset" class="btn btn-primary">Reset</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div><!-- /.col-->
-    </div>
-</section><!-- /.col-->
+  												<button class="btn btn-primary" type="reset">Reset</button>
+  												<button type="submit" id="submit" class="btn btn-success">Modify</button>
+  											</div>
+  										</div>
 
+
+  										</div>
+  										<div class="ln_solid"></div>
+
+
+  									</form>
+  								</div>
+  							</div>
+  						</div>
 
 @endsection()
