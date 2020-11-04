c226348edf1280dc86bd641805ce4022d06b7483
yves <kabiryves90@gmail.com>
2020-11-04 13:45:11 +0200

    modify views modeles index

--- resources/views/modeles/index.blade.php
+++ resources/views/modeles/index.blade.php
@@ -65,30 +65,31 @@
   							</div>
   						</div>
 
+
+
               <div class="col-md-12 col-sm-12 ">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Liste des modeles</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                              <div class="x_panel">
+                                <div class="x_title">
+                                  <h2>Liste des modeles</small></h2>
+                                  <ul class="nav navbar-right panel_toolbox">
+                                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                    </li>
+                                    <li class="dropdown">
+                                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
 
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                      <div class="row">
-                          <div class="col-sm-12">
-                            <div class="card-box table-responsive">
-                    <p class="text-muted font-13 m-b-30">
+                                    </li>
+                                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                                    </li>
+                                  </ul>
+                                  <div class="clearfix"></div>
+                                </div>
+                                <div class="x_content">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                          <div class="card-box table-responsive">
 
-                    </p>
-                    <table id="datatable-buttons" class="table table-striped table-bordered" style="width:100%">
+                                  <table id="datatable-fixed-header" class="table table-striped table-bordered" style="width:100%">
+
                       <thead>
                     <tr>
                         <th>ID</th>
