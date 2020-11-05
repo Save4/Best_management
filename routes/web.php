691585bdb77adacd13b1f7dcd2cc8fed42740af5
yves <kabiryves90@gmail.com>
2020-11-05 11:50:44 +0200

    modification des route du marque

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,12 @@ Route::post('modeles','ModelesController@store');
 Route::get('modeles/edit/{Modele}','ModelesController@edit');
 Route::put('modeles/{Modele}','ModelesController@update');
 Route::post('modeles/destroy/{Modele}','ModelesController@destroy');
+
+//marque
+Route::get('marques','MarquesController@index');
+Route::get('marques/create','MarquesController@create');
+Route::post('marques','MarquesController@store');
+Route::get('marques/edit/{Marque}','MarquesController@edit');
+Route::get('marques/show/{Modele}','MarquesController@show');
+Route::put('marques/{Marque}','MarquesController@update');
+Route::post('marques/destroy/{Marque}','MarquesController@destroy');
