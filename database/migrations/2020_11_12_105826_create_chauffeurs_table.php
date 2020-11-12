671643a7ab8ee866f65ede022297d1b1c687810b
yves <kabiryves90@gmail.com>
2020-11-12 13:43:41 +0200

    add column in table chauffeur

--- database/migrations/2020_11_12_105826_create_chauffeurs_table.php
+++ database/migrations/2020_11_12_105826_create_chauffeurs_table.php
@@ -14,8 +14,13 @@ class CreateChauffeursTable extends Migration
     public function up()
     {
         Schema::create('chauffeurs', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+          $table->bigIncrements('id');
+          $table->string('nom');
+          $table->string('prenom');
+          $table->string('email');
+          $table->string('telephone');
+         $table->string('id_permis_conduire');
+         $table->timestamps();
         });
     }
 
