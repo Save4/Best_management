5fed79b149b1b227b67730b1e5139946fe8276c3
yves <kabiryves90@gmail.com>
2020-11-17 12:42:16 +0200

    add column in reparation table

--- database/migrations/2020_11_17_103056_create_reparations_table.php
+++ database/migrations/2020_11_17_103056_create_reparations_table.php
@@ -14,8 +14,34 @@ class CreateReparationsTable extends Migration
     public function up()
     {
         Schema::create('reparations', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->bigIncrements('id');
+         $table->unsignedBigInteger('vehicule_id');
+         $table->unsignedBigInteger('chauffeur_id');
+         $table->unsignedBigInteger('fournisseur_id');
+        $table->string('type_reparation');
+        $table->string('piece');
+        $table->integer('nombre_piece');
+        $table->integer('prix_unitaire');
+        $table->integer('prix_total');
+        $table->integer('main_oeuvre');
+        $table->integer('montant_total');
+        $table->string('monaie');
+        $table->timestamps();
+        $table->foreign('vehicule_id')
+               ->references('id')
+               ->on('vehicules')
+               ->onUpdate('cascade')
+               ->onDelete('cascade');
+        $table->foreign('chauffeur_id')
+               ->references('id')
+               ->on('chauffeurs')
+               ->onUpdate('cascade')
+               ->onDelete('cascade');
+       $table->foreign('fournisseur_id')
+               ->references('id')
+               ->on('fournisseurs')
+               ->onUpdate('cascade')
+               ->onDelete('cascade');
         });
     }
 
