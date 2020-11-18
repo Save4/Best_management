37207f79a150ca54cd6fc093723c4b9122fbd066
yves <kabiryves90@gmail.com>
2020-11-18 12:57:57 +0200

    ajout des operation dans reparation

--- app/Http/Controllers/ReparationsController.php
+++ app/Http/Controllers/ReparationsController.php
@@ -1,85 +1,177 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Reparation;
+use App\Vehicule;
+use App\Fournisseur;
+use App\Chauffeur;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ReparationsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+
     public function index()
-    {
-        //
-    }
+{
+    $reparations= DB::table('reparations')
+           ->join('vehicules', 'reparations.vehicule_id', 'vehicules.id')
+           ->join('chauffeurs', 'reparations.chauffeur_id', 'chauffeurs.id')
+           ->join('fournisseurs', 'reparations.fournisseur_id', 'fournisseurs.id')
+           ->select('vehicules.*','chauffeurs.*','fournisseurs.*','reparations.*')
+           ->get();
+           $vehicules = Vehicule::all();
+           $chauffeurs = Chauffeur::all();
+           $fournisseurs = Fournisseur::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  return view('reparations/index',[
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    'reparations'=> $reparations,
+    'vehicules'=> $vehicules,
+    'chauffeurs'=> $chauffeurs,
+    'fournisseurs'=>$fournisseurs
+  ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Reparation  $reparation
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Reparation $reparation)
-    {
-        //
-    }
+}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Reparation  $reparation
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Reparation $reparation)
-    {
-        //
-    }
+public function create()
+{
+    $vehicules = Vehicule::all();
+    $chauffeurs = Chauffeur::all();
+    $fournisseurs = Fournisseur::all();
+     return view('reparations/create',[
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Reparation  $reparation
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Reparation $reparation)
-    {
-        //
-    }
+    'vehicules'=> $vehicules,
+    'chauffeurs'=> $chauffeurs,
+    'fournisseurs'=>$fournisseurs
+        ]);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Reparation  $reparation
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Reparation $reparation)
-    {
-        //
-    }
+
+
+     }
+
+public function store(Request $request)
+{
+
+     //validation
+     $request->validate([
+         'vehicule_id' =>'required',
+         'chauffeur_id' =>'required',
+         'fournisseur_id' =>'required',
+         'type_reparation'=>'required',
+         'piece' =>'required',
+         'nombre_piece' =>'required',
+         'prix_unitaire' =>'required',
+         'main_oeuvre' =>'required',
+         'monaie' =>'required'
+
+     ]);
+
+     $Reparation= new Reparation();
+     $Reparation->vehicule_id= $request->vehicule_id;
+     $Reparation->chauffeur_id= $request->chauffeur_id;
+     $Reparation->fournisseur_id= $request->fournisseur_id;
+     $Reparation->type_reparation= $request->type_reparation;
+     $Reparation->piece= $request->piece;
+     $Reparation->nombre_piece= $request->nombre_piece;
+     $Reparation->prix_unitaire= $request->prix_unitaire;
+     $Reparation->prix_total= $request->nombre_piece*$request->prix_unitaire;
+     $Reparation->main_oeuvre= $request->main_oeuvre;
+     $Reparation->montant_total= ($request->nombre_piece*$request->prix_unitaire)+$request->main_oeuvre;
+     $Reparation->monaie= $request->monaie;
+
+     $Reparation->save();
+     return redirect('reparations');
+}
+
+public function show()
+{
+ $reparations= DB::table('reparations')
+           ->join('vehicules', 'reparations.vehicule_id', 'vehicules.id')
+           ->join('chauffeurs', 'reparations.chauffeur_id', 'chauffeurs.id')
+           ->join('fournisseurs', 'reparations.fournisseur_id', 'fournisseurs.id')
+           ->select('vehicules.*','chauffeurs.*','fournisseurs.*','reparations.*')
+           ->get();
+           $vehicules = Vehicule::all();
+           $chauffeurs = Chauffeur::all();
+           $fournisseurs = Fournisseur::all();
+
+return view('reparations/show',[
+
+'reparations'=> $reparations,
+    'vehicules'=> $vehicules,
+    'chauffeurs'=> $chauffeurs,
+    'fournisseurs'=>$fournisseurs
+]);
+
+}
+
+public function edit(Reparation $Reparation)
+{
+    # code...
+   $vehicules = Vehicule::all();
+   $chauffeurs = Chauffeur::all();
+   $fournisseurs = Fournisseur::all();
+
+   $Reparation=Reparation::find($Reparation->id);
+
+    return view('reparations/edit',[
+    'Reparation'=> $Reparation,
+    'vehicules'=> $vehicules,
+    'chauffeurs'=> $chauffeurs,
+    'fournisseurs'=>$fournisseurs
+
+
+    ]);
+
+ }
+
+ public function update(Request $request, Reparation $Reparation)
+ {
+     # code...
+     $request->validate([
+       'vehicule_id' =>'required',
+         'chauffeur_id' =>'required',
+         'fournisseur_id' =>'required',
+         'type_reparation'=>'required',
+         'piece' =>'required',
+         'nombre_piece' =>'required',
+         'prix_unitaire' =>'required',
+         'main_oeuvre' =>'required',
+         'monaie' =>'required'
+
+     ]);
+
+    $Reparation->vehicule_id= $request->vehicule_id;
+     $Reparation->chauffeur_id= $request->chauffeur_id;
+     $Reparation->fournisseur_id= $request->fournisseur_id;
+     $Reparation->type_reparation= $request->type_reparation;
+     $Reparation->piece= $request->piece;
+     $Reparation->nombre_piece= $request->nombre_piece;
+     $Reparation->prix_unitaire= $request->prix_unitaire;
+     $Reparation->prix_total= $request->nombre_piece*$request->prix_unitaire;
+     $Reparation->main_oeuvre= $request->main_oeuvre;
+     $Reparation->montant_total= ($request->nombre_piece*$request->prix_unitaire)+$request->main_oeuvre;
+     $Reparation->monaie= $request->monaie;
+
+    $Reparation->save();
+    return redirect('reparations');
+
+ }
+
+ public function destroy(Reparation $Reparation)
+ {
+     # code...
+     $Reparation= Reparation::find($Reparation->id);
+     $Reparation->delete();
+     return redirect('reparations');
+
+ }
+
+
 }
