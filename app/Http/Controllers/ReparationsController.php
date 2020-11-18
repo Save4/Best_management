f579977586b83971ce5dc25adf22ac5678bf5ca7
yves <kabiryves90@gmail.com>
2020-11-18 12:51:42 +0200

    add controller reparation

---
+++ app/Http/Controllers/ReparationsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reparation;
+use Illuminate\Http\Request;
+
+class ReparationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Reparation  $reparation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reparation $reparation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Reparation  $reparation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Reparation $reparation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Reparation  $reparation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reparation $reparation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Reparation  $reparation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reparation $reparation)
+    {
+        //
+    }
+}
