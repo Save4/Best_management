f3088b119dc18dae25a8cec39dbee61c4b0bdb42
yves <kabiryves90@gmail.com>
2020-11-18 12:44:52 +0200

    add controller autre consommation

---
+++ app/Http/Controllers/Autre_consommationsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Autre_consommation;
+use Illuminate\Http\Request;
+
+class Autre_consommationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Autre_consommation  $autre_consommation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Autre_consommation $autre_consommation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Autre_consommation  $autre_consommation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Autre_consommation $autre_consommation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Autre_consommation  $autre_consommation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Autre_consommation $autre_consommation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Autre_consommation  $autre_consommation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Autre_consommation $autre_consommation)
+    {
+        //
+    }
+}
