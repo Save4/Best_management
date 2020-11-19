b76427b5c3b3d6dfc5bbcf81278455b836f976a9
yves <kabiryves90@gmail.com>
2020-11-19 18:20:29 +0200

    modify app/Providers/AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,22 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('manage-users', function ($user) {
+       return $user->hasAnyRole(['DG']);
+   });
+
+
+   Gate::define('edit-users', function ($user) {
+       return $user->isAdmin();
+      // return $user->hasAnyRole(['auteur','admin']);
+   });
+    Gate::define('delete-users', function ($user) {
+       return $user->isAdmin();
+   });
+
+  // Gate::define('update-post', function ($user, $post) {
+     //  return $user->id === $post->user_id;
+  // });
+   
     }
 }
