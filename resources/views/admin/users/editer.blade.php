c76d22ac0bf117b8e21ba0f47d84253cac4eb3cd
yves <kabiryves90@gmail.com>
2020-11-19 18:18:07 +0200

    modify admin.user.edit

--- resources/views/admin/users/editer.blade.php
+++ resources/views/admin/users/editer.blade.php
@@ -1,7 +1,6 @@
-@extends('templates.default_loyout')
-
-
+@extends('layouts.layout')
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
