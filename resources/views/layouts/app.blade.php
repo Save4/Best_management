ac96a9de005802e5849818c98a62b5c82949be31
yves <kabiryves90@gmail.com>
2020-11-28 15:40:43 +0200

    modifier title login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'best_management') }}</title>
+    <title>@yield('title',env('APP_NAME')) </title>
+    
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
