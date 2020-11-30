f5409ee754ab3e61dbf2134e2eeb3b5ad12338aa
yves <kabiryves90@gmail.com>
2020-11-30 09:10:50 +0200

    modify title modele

--- resources/views/modeles/edit.blade.php
+++ resources/views/modeles/edit.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.layout')
 @section('content')
+  @section('title','Modele | '    config('app.name'))
+
+
 
   <div class="clearfix"></div>
   					<div class="row">
@@ -10,7 +13,7 @@
   									<ul class="nav navbar-right panel_toolbox">
   										<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
   										</li>
-  										
+
   										<li><a class="close-link"><i class="fa fa-close"></i></a>
   										</li>
   									</ul>
